a238d937e99554639e489b02d46f81d9c278b78a
Rowan Ibrahim <rowan.ibrahim@yomicepa.com>
2020-03-02 18:53:59 +0200

    create rest controller

--- HR-SYSTEM/src/main/java/com/task/hrsystem/controller/EmployeeController.java
+++ HR-SYSTEM/src/main/java/com/task/hrsystem/controller/EmployeeController.java
@@ -1,7 +1,6 @@
 package com.task.hrsystem.controller;
 
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
