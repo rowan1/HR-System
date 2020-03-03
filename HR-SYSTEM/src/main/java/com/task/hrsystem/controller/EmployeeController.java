0c4ee29587f49ac3d9929be9f8ad6087005cf953
Rowan Ibrahim <rowan.ibrahim@yomicepa.com>
2020-03-03 17:03:04 +0200

    deleteing old package

--- HR-SYSTEM/src/main/java/com/task/hrsystem/controller/EmployeeController.java
+++
@@ -1,9 +0,0 @@
-package com.task.hrsystem.controller;
-
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestParam;
-
-@RestController
-public class EmployeeController {
-
-}
